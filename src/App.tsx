f6dbf642bc3d639ed86e0bec5c120f23e4fcc09b
amongDOHEE <dohee@sandboxnetwork.net>
2022-12-06 18:12:28 +0900

    modify: add dYdX1 & dYdX2 svg tspan property

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BubbleChart from "./BubbleChart/BubbleChart";
 
 function App() {
+  const data = [
+    {
+      fillColor: "rgb(52, 202, 173, 0.3)",
+      id: 0,
+      name: "블랙핑크\n지도",
+      size: 120,
+    },
+    {
+      theme: "white",
+      fillColor: "white",
+      id: 1,
+      name: "workit",
+      size: 120,
+    },
+    {
+      fillColor: "rgb(52, 202, 173, 0.3)",
+      id: 2,
+      name: "안녕\n어렵다",
+      size: 120,
+    },
+    {
+      theme: "white",
+      fillColor: "white",
+      id: 3,
+      name: "블랙핑크\n아오오",
+      size: 120,
+      dYdX1: { dy: 0, dx: 10 },
+    },
+    {
+      fillColor: "rgb(52, 202, 173, 0.3)",
+      id: 4,
+      name: "want to\nspace it",
+      size: 120,
+      dYdX2: { dy: 12, dx: -50 },
+    },
+    {
+      fillColor: "rgb(52, 202, 173, 0.3)",
+      id: 5,
+      name: "일본\n경제",
+      size: 120,
+    },
+    {
+      fillColor: "rgb(52, 202, 173, 0.3)",
+      id: 5,
+      name: "일본\n테스트로",
+      size: 120,
+    },
+    {
+      fillColor: "rgb(52, 202, 173, 0.3)",
+      id: 5,
+      name: "한국\n성공안녕",
+      size: 120,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BubbleChart
+        key={`bubble-chart`}
+        bubblesData={data}
+        width={700}
+        height={470}
+        textFillColor="#717C84"
+        backgroundColor="white"
+        minValue={1}
+        maxValue={150}
+        move={false}
+      />
     </div>
   );
 }
