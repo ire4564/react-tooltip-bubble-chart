6050cdf5424111ef95a1e88bd3fc675a3071ac87
ire4564 <ire4564@gmail.com>
2022-12-10 13:15:35 +0900

    feat: setting npm package

---
+++ storybook/stories/index.js
@@ -0,0 +1,9 @@
+import * as BubbleChart from "./BubbleChart/BubbleChart";
+import * as BubbleChartStyle from "./BubbleChart/BubbleChartStyle";
+import * as BubbleTooltip from "./BubbleChart/BubbleTooltip";
+import * as SvgAlignEN from "./BubbleChart/SvgAlignEN";
+import * as SvgAlignKR from "./BubbleChart/SvgAlignKR";
+
+export { BubbleChart, BubbleTooltip, BubbleChartStyle, SvgAlignEN, SvgAlignKR };
+
+export * from "./BubbleChart";
