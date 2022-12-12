151acaaf5f8ffaf8afa7cdbed36e10ece19c770c
ire4564 <ire4564@gmail.com>
2022-12-12 11:12:44 +0900

    modify: clean files

---
+++ storybook/index.js
@@ -0,0 +1,9 @@
+import * as BubbleChart from "./stories/BubbleChart/BubbleChart";
+import * as BubbleChartStyle from "./BubbleChart/BubbleChartStyle";
+import * as BubbleTooltip from "./stories/BubbleChart/BubbleTooltip";
+import * as SvgAlignEN from "./stories/BubbleChart/SvgAlignEN";
+import * as SvgAlignKR from "./stories/BubbleChart/SvgAlignKR";
+
+export { BubbleChart, BubbleTooltip, BubbleChartStyle, SvgAlignEN, SvgAlignKR };
+
+export * from "./BubbleChart";
