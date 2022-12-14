4d6703e03563c94ba2bcc020e453235a16d5ab88
ire4564 <ire4564@gmail.com>
2022-12-14 18:22:31 +0900

    modify: update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./lib/index.js",
+  entry: "./lib/index.ts",
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "index.d.ts",
